b330e0900d02edf7b32a0153cb2523e8ae977c55
liuyuanmoon <528237744@qq.com>
2019-06-22 08:03:53 +0800

    aed

---
+++ js/water.js
@@ -0,0 +1,24 @@
+window.onload=function () {
+    let mySwiper = new Swiper ('.swiper-container', {
+
+        loop: true, // 循环模式选项
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+       autoplay:{
+            delay:2500,
+           disableOnInterraction:false,
+       },
+
+
+
+    })
+}
